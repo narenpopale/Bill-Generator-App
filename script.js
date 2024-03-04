836454ecb44f1232c3cf3988d2e8c81a5d66b8f8
naren <narenpopale@gmail.com>
2024-03-04 15:52:00 +0530

    feat: made storage feature

--- script.js
+++ script.js
@@ -1,45 +1,98 @@
 
-var bill = document.querySelector("#bill");
-var people = document.querySelector("#people");
-var Damount = document.querySelector("#Discount-Amount");
-var Tamount = document.querySelector("#Total-Amount");
-var calculate = document.querySelector("#calc");
-var reset = document.querySelector("#reset");
-var span = document.querySelector('span');
-var disc;
+// ----------------Company Info and Customer Info---------------
+
+// Company
+var companyName = document.querySelector('#Company');
+var companyPhone = document.querySelector('#CompanyPhone');
+var saveCompanyInfo = document.querySelector('#company-info');
+var cName = document.querySelector('h1');
+var companyInfo = {
+    'name': 'Company Name',
+    'phone': '123'
+}
 
 
-var person_amount = 0;
-var no_of_people = 0;
-var discount_percent = 0;
+// Customer
+var customerName = document.querySelector('#Name');
+var customerPhone = document.querySelector('#Phone');
+var customerAddress = document.querySelector('#Address');
+var saveCustomerInfo = document.querySelector('#customer-info');
+var customerInfo = {
+    'name': 'xyz',
+    'phone': '123',
+    'address': 'abc'
+}
 
 
-calculate.addEventListener('click', function () {
-    person_amount = bill.value;
-    no_of_people = people.value;
+// updating UI
+setInterval(() => {
+    cName.textContent = companyInfo.name;
+}, 1000);
 
-    if (discount_percent == 0) {
-        discount_percent = disc.value;
-    }
 
-    let d = (person_amount / no_of_people) * (discount_percent / 100);
-    let t = person_amount / no_of_people;
-    Damount.textContent = `$${d.toFixed(2)}`;
-    Tamount.textContent = `$${t.toFixed(2)}`;
+saveCompanyInfo.addEventListener('click', () => {
+    companyInfo.name = companyName.value;
+    companyInfo.phone = companyPhone.value;
+    console.log(companyInfo);
+    localStorage.setItem('company-info', JSON.stringify(companyInfo));
 })
 
 
-// Discount Logic
+saveCustomerInfo.addEventListener('click', () => {
+    customerInfo.name = customerName.value;
+    customerInfo.phone = customerPhone.value;
+    customerInfo.address = customerAddress.value;
+    console.log(customerInfo);
+    localStorage.setItem('customer-info', JSON.stringify(customerInfo));
+})
+
+
+
+// Reset Button
+var resetInfo = document.querySelector('#reset-info');
+
+resetInfo.addEventListener('click', () => {
+    companyName.value = "";
+    companyPhone.value = "";
+    customerName.value = "";
+    customerPhone.value = "";
+    customerAddress.value = "";
+    companyInfo.name = "Company Name";
+    localStorage.clear();
+})
+
+
+
+
+// ----------------Left Bill Container Logic---------------------
+
+var item_name = document.querySelector("#product");
+var unit_price = document.querySelector('#bill');
+var quantity = document.querySelector("#people");
+var total_items = document.querySelector("#Discount-Amount");
+var total_price = document.querySelector("#Total-Amount");
+var add = document.querySelector("#calc");
+var reset = document.querySelector("#reset");
+var clear_all = document.querySelector("#clear");
+var span = document.querySelectorAll('span');
+
+
+var discount_percent = 0;
+
+
+
+// Discount Logic ----->
 var customDiscount = document.querySelector("#Custom");
 var discount = document.querySelectorAll(".rect");
+var customDiscountActive;
 var isClicked = false;
 
 customDiscount.addEventListener('click', function () {
     if (!isClicked) {
         removeRectClass(discount);
         customDiscount.innerHTML = '<input type="number" style="margin:0px" name="discount" id="discount" value="0">';
+        customDiscountActive = document.querySelector('#discount');
         isClicked = true;
-        disc = document.querySelector("#discount");
     }
     discount_percent = 0;
 })
@@ -73,34 +126,55 @@ function removeRectClass(discount) {
         }
     })
 }
+// Discount Logic ----->
 
 
 
+// Reset Logic
 reset.addEventListener('click', function () {
-    bill.value = 0;
-    people.value = 0;
+    item_name.value = "";
+    unit_price.value = 0;
+    quantity.value = 0;
 
     removeRectClass(discount);
 
-    Damount.textContent = `$0.00`;
-    Tamount.textContent = `$0.00`;
+    total_items.textContent = `0`;
+    total_price.textContent = `$0.00`;
 })
 
 
 
 // Validation Logic
-setInterval(function(){
-    no_of_people = people.value;
+setInterval(function () {
 
-    if(no_of_people == 0) {
-        span.style.visibility = "visible";
-        calculate.setAttribute('disabled', true);
-        reset.setAttribute('disabled', true);
+    // updating discount percent
+    if (isClicked) discount_percent = customDiscountActive.value;
+
+    let name, items;
+
+    if (item_name.value == "") {
+        span[0].style.visibility = "visible";
+        name = false;
     }
     else {
-        span.style.visibility = "hidden";
-        calculate.removeAttribute('disabled', true);
-        reset.removeAttribute('disabled', true);
+        span[0].style.visibility = "hidden";
+        name = true;
     }
-    
+
+    if (quantity.value == 0) {
+        span[1].style.visibility = "visible";
+        items = false;
+    }
+    else {
+        span[1].style.visibility = "hidden";
+        items = true;
+    }
+
+    if (name && items) {
+        add.removeAttribute('disabled', true);
+    }
+    else {
+        add.setAttribute('disabled', true);
+    }
+
 }, 1000);
